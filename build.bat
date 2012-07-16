805a8c60140c4a199c95b157a5ccd55fb650c267
Christian Heimes <christian@cheimes.de>
2012-07-16 14:53:27 +0200

    Updated Windows build helper script

--- build.bat
+++ build.bat
@@ -2,11 +2,13 @@
 cls
 setlocal
 rem set PATH=%PATH%;%ProgramFiles%\mingw32\bin;%ProgramFiles%\msys\bin
-set PYTHONPATH=src
+set PYTHONPATH=%~d0%~p0
+set PYTHON=..\..\environment\Python27\python.exe
 
-del src\smc\freeimage\_freeimage.c
-del src\smc\freeimage\*.pyd
-..\..\environment\Python27\python.exe setup.py build_ext -i
-..\..\environment\Python27\python.exe -m smc.freeimage.tests.__main__
-rem ..\..\environment\Python26\python.exe -m smc.freeimage.tests.test_lcms
+del smc\freeimage\_freeimage.c
+del smc\freeimage\*.pyd
+
+%PYTHON% setup.py build_ext -i
+%PYTHON% -c "import smc.freeimage; print(smc.freeimage)"
+%PYTHON% -m smc.freeimage.tests.__main__
 pause

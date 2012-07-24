e4a50554cecf8dfb0748e41de5ee02125203439b
Christian Heimes <christian@cheimes.de>
2012-07-25 00:03:57 +0200

    benchmark resize filters and buffer to numpy array

--- contrib/benchmark.py
+++ contrib/benchmark.py
@@ -1,11 +1,16 @@
 from smc.freeimage import Image, hasJPEGTurbo, getVersion
 from smc.freeimage.enums import FI_FILTER
 from PIL.Image import open as pil_open, VERSION as PIL_VERSION, ANTIALIAS, BICUBIC, BILINEAR, NEAREST
-from smc.freeimage.tests.common import IMG
+from smc.freeimage.tests.common import IMG, BITON, TIFF
 
 from time import time
 import sys
-import os
+
+try:
+    from scipy.misc.pilutil import fromimage, imread, bytescale
+    from numpy import asarray
+except ImportError:
+    print("No numpy and scipy")
 
 RW_COUNT = 300
 RESIZE_COUNT = 50
@@ -13,7 +18,9 @@ RESIZE_COUNT = 50
 print "Python %i.%i.%i" % sys.version_info[:3]
 print "read / write cycles: %i" % RW_COUNT
 print "resize cycles: %i" % RESIZE_COUNT
-print repr(Image(IMG))
+print "JPEG: %r" % Image(IMG)
+print "TIFF LZW: %r" % Image(TIFF)
+print "TIFF bitonal G4: %r" % Image(BITON)
 print ""
 
 if hasJPEGTurbo():
@@ -27,7 +34,7 @@ start = time()
 for i in xrange(RW_COUNT):
     Image(IMG)
 end = time() - start
-print " - read %0.3f sec" % end
+print " - read JPEG %0.3f sec" % end
 
 # load resaved
 # http://www.libjpeg-turbo.org/About/Performance Restart Markers
@@ -36,7 +43,20 @@ start = time()
 for i in xrange(RW_COUNT):
     Image("pon_rewrite_fi.jpg")
 end = time() - start
-print " - read %0.3f sec (resaved)" % end
+print " - read JPEG %0.3f sec (resaved)" % end
+
+# TIFF read
+start = time()
+for i in xrange(RW_COUNT):
+    Image(TIFF)
+end = time() - start
+print " - read LZW TIFF %0.3f sec" % end
+
+start = time()
+for i in xrange(RW_COUNT):
+    Image(BITON)
+end = time() - start
+print " - read biton G4 TIFF %0.3f sec" % end
 
 # save
 img = Image(IMG)
@@ -44,7 +64,7 @@ start = time()
 for i in xrange(RW_COUNT):
     img.save("testfi.jpg")
 end = time() - start
-print " - write %0.3f sec" % end
+print " - write JPEG %0.3f sec" % end
 
 # resize
 width = img.width // 2
@@ -53,7 +73,7 @@ filters = (("box", FI_FILTER.FILTER_BOX),
            ("bilinear", FI_FILTER.FILTER_BILINEAR),
            ("bspline", FI_FILTER.FILTER_BSPLINE),
            ("bicubic", FI_FILTER.FILTER_BICUBIC),
-           ("catmullrom", FI_FILTER.FILTER_CATMULLROM),
+           ("catmull rom spline", FI_FILTER.FILTER_CATMULLROM),
            ("lanczos3", FI_FILTER.FILTER_LANCZOS3))
 for name, flt in filters:
     start = time()
@@ -62,14 +82,31 @@ for name, flt in filters:
     end = time() - start
     print " - resize %0.3f sec (%s)" % (end, name)
 
-# ---
+# numpy
+tiff = Image(TIFF)
+start = time()
+for i in xrange(RW_COUNT):
+    arr = asarray(tiff)
+    bytescale(arr, 64, 192)
+end = time() - start
+print " - tiff numpy.asarray() with bytescale() %0.3f sec" % end
+
+start = time()
+for i in xrange(RW_COUNT):
+    tiff = Image(TIFF)
+    arr = asarray(tiff)
+    bytescale(arr, 64, 192)
+end = time() - start
+print " - tiff load + numpy.asarray() with bytescale() %0.3f sec" % end
+
+# -------------------------------------------------------------------
 # PIL load
 print "PIL %s" % PIL_VERSION
 start = time()
 for i in xrange(RW_COUNT):
     pil_open(IMG).load()
 end = time() - start
-print " - read %0.3f sec" % end
+print " - read JPEG %0.3f sec" % end
 
 # load resaved
 pil_open(IMG).save("pon_rewrite_pil.jpg")
@@ -77,7 +114,16 @@ start = time()
 for i in xrange(RW_COUNT):
     pil_open("pon_rewrite_pil.jpg").load()
 end = time() - start
-print " - read %0.3f sec (resaved)" % end
+print " - read JPEG %0.3f sec (resaved)" % end
+
+# TIFF read
+start = time()
+for i in xrange(RW_COUNT):
+    pil_open(TIFF).load()
+end = time() - start
+print " - read LZW TIFF %0.3f sec" % end
+
+print " - read biton G4 TIFF: decoder group4 not available"
 
 # write
 img = pil_open(IMG)
@@ -86,7 +132,7 @@ start = time()
 for i in xrange(RW_COUNT):
     img.save("testpil.jpg")
 end = time() - start
-print " - write %0.3f sec" % end
+print " - write JPEG %0.3f sec" % end
 
 # resize
 filters = (("nearest", NEAREST), ("bilinear", BILINEAR),
@@ -97,3 +143,20 @@ for name, flt in filters:
         img.resize((width, height), flt)
     end = time() - start
     print " - resize %0.3f sec (%s)" % (end, name)
+
+# scipy
+tiff = pil_open(TIFF)
+tiff.load()
+start = time()
+for i in xrange(RW_COUNT):
+    arr = fromimage(tiff)
+    bytescale(arr, 64, 192)
+end = time() - start
+print " - tiff scipy fromimage() with bytescale() %0.3f sec" % end
+
+start = time()
+for i in xrange(RW_COUNT):
+    arr = imread(TIFF)
+    bytescale(arr, 64, 192)
+end = time() - start
+print " - tiff scipy imread() with bytescale() %0.3f sec" % end
